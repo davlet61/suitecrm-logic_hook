688518974a409cf1f8c888d656fbc15bec26a2d2
Dovlat <dovlat93@gmail.com>
2022-09-21 00:02:27 +0200

    refactors invoice hook

--- invoiceHooks.php
+++ invoiceHooks.php
@@ -9,7 +9,6 @@ $dotenv->loadEnv(dirname(__DIR__, 5).'/.env');
 
 class InvoiceRequests 
     {
-        private $output;
         private $expires;
         private $tokens;
         private $app_key;
@@ -22,10 +21,11 @@ class InvoiceRequests
                 $this->logger = LoggerManager::getLogger();
                 $this->app_key = $_ENV['PO_APP_KEY'];
                 $this->client_key = $_ENV['PO_CLIENT_KEY'];
-                $this->url = $_ENV['PO_URL'];
+                $this->url = $_ENV['POWERAPI_URL'];
                 $ch = curl_init();
+                
                 $curlopts = array(
-                    CURLOPT_URL => "$this->url/oauth",
+                    CURLOPT_URL => "$this->url/poweroffice/oauth",
                     CURLOPT_RETURNTRANSFER => true,
                     CURLOPT_ENCODING => "",
                     CURLOPT_MAXREDIRS => 10,
@@ -38,12 +38,12 @@ class InvoiceRequests
                     ),
                 );
                 curl_setopt_array($ch, $curlopts);
-                $this->output = curl_exec($ch);
-                if ($this->output === false) {
-                    $this->logger->fatal("Curl error: " . curl_error($ch));
+                $output = curl_exec($ch);
+                if ($output === false) {
+                    $this->logger->fatal("43 => Curl error: " . curl_error($ch));
                 }
-                curl_close($this->ch);
-                $this->tokens = json_decode($this->output, true);
+                curl_close($ch);
+                $this->tokens = json_decode($output, true);
                 $this->expires = microtime(true) + $this->tokens['expires_in'];
             }
 
@@ -52,7 +52,7 @@ class InvoiceRequests
                 $ch = curl_init();
                 $escapedParams = curl_escape($ch, $name);
                 $curlopts = array(
-                    CURLOPT_URL => "$this->url/customers?name=$escapedParams",
+                    CURLOPT_URL => "$this->url/poweroffice/customers?name=$escapedParams",
                     CURLOPT_RETURNTRANSFER => true,
                     CURLOPT_HTTPHEADER => array("access_token: ".$this->tokens['access_token'])
                 );
@@ -60,11 +60,39 @@ class InvoiceRequests
                 $output = curl_exec($ch);
 
                 if ($output === false) {
-                    $this->logger->fatal("Curl error: " . curl_error($ch));
+                    $this->logger->fatal("59 => Curl error: " . curl_error($ch));
                 }
                 curl_close($ch);
                 return json_decode($output, true);
             }
+            
+        private function createCustomer($data)
+            {
+                $accessToken = $this->tokens['access_token'];
+                $ch = curl_init();
+                $curlopts = array(
+                    CURLOPT_URL => "$this->url/poweroffice/customers",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "POST",
+                    CURLOPT_POSTFIELDS => $data,
+                    CURLOPT_HTTPHEADER => array(
+                        "access_token: $accessToken",
+                        "content-type: application/json",
+                    )
+                );
+                curl_setopt_array($ch, $curlopts);
+                $output = curl_exec($ch);
+
+                if ($output === false) {
+                    $this->logger->fatal("91 => Curl error: " . curl_error($ch));
+                }
+                curl_close($ch);
+                return json_decode($output, true);
+            }            
 
         // AFTER SAVE
         public function createInvoiceInPO(&$bean, $event, $arguments)
@@ -75,23 +103,87 @@ class InvoiceRequests
                 $name= $account->name ?? $contact->name;
                 $customer = self::getCustomerByName($name);
                 $customerCode = $customer['data'][0]['code'];
-
+                
+                
+                
+                // Create Customer in PO if no existing
+                if ($customer['count'] === 0) {
+                    $customerData = array(
+                        "invoiceDeliveryType" => $account->email1 ? 0 : 2,
+                        "isVatFree" => false,
+                        "invoiceEmailAddress" => $account->email1,
+                        "invoiceEmailAddressCC" => "",
+                        "useFactoring" => false,
+                        "sendReminders" => true,
+                        "doNotAddLatePaymentFees" => false,
+                        "doNotAddLatePaymentInterest" => false,
+                        "reminderEmailAddress" => "",
+                        "transferToDebtCollectionAgency" => true,
+                        "useInvoiceFee" => true,
+                        "name" => $account->name,
+                        "vatNumber" => $account->phone_fax,
+                        "since" => date('Y-m-j'),
+                        "isPerson" => false,
+                        "isActive" => true,
+                        'mailAddress' => array(
+                            'address1' => $account->billing_address_street,
+                            'zipCode' => $account->billing_address_postalcode,
+                            'city' => strtoupper($account->billing_address_city),
+                        ),
+                        "streetAddress" => array(
+                            'address1' => $account->shipping_address_street,
+                            'zipCode' => $account->shipping_address_postalcode,
+                            'city' => strtoupper($account->shipping_address_city),
+                        ),
+                        "streetAddresses" => array(),
+                        "emailAddress" => $primary,
+                        "isArchived" => false,
+                        "phoneNumber" => $account->phone_office,
+                        "contactGroups" => array()
+                    );
+                    
+                    $payload = json_encode($customerData);
+                    
+                    $poCustomer = self::createCustomer($payload);
+                    $customerCode = $poCustomer['data']['code'];
+                }
+                
+                $items = array();
+                
+                $link = 'aos_products_quotes';
+                
+                if ($bean->load_relationship($link)) {
+                    $lineItems = $bean->$link->getBeans();
+                    
+                    foreach ($lineItems as $lineItem) {
+                        $product = BeanFactory::getBean('AOS_Products', $lineItem->product_id);
+                        $productItem = array(
+                            "description" => $lineItem->item_description,
+                            "discountPercent" => $lineItem->product_discount,
+                            "productCode" => $product->maincode,
+                            "quantity" => $lineItem->product_qty,
+                            "unitCost" => $lineItem->product_unit_price,
+                            "unitPrice" => $lineItem->product_list_price,
+                            "vatRate" => $lineItem->vat,
+                        );
+                        array_push($items, $productItem);
+                    }
+                    
+                }
+                
                 $invoice = array(
-                    "id" => $bean->id,
                     "invoiceDeliveryType" => 0,
                     "customerCode" => $customerCode,
-                    "totalAmount" => 0,
-                    "netAmount" => 0,
-                    "status" => 0,
+                    "totalAmount" => $bean->total_amount,
                     "paymentTerms" => 14,
-                    "outgoingInvoiceLines" => [],
+                    "outgoingInvoiceLines" => $items,
                     "isInvoiceBeingProcessed" => false
-                    );
+                );
 
                 $curl = curl_init();
                 $payload = json_encode($invoice);
                 curl_setopt_array($curl, array(
-                    CURLOPT_URL => "$this->url/invoices",
+                    CURLOPT_URL => "$this->url/poweroffice/invoices",
                     CURLOPT_RETURNTRANSFER => true,
                     CURLOPT_ENCODING => "",
                     CURLOPT_MAXREDIRS => 10,
